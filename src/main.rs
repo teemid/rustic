e2f7d0bf8340565c5a21e4d7af89c09d5435ef6b
Emil Grunt <emilgrunt@gmail.com>
2017-02-03 17:54:27 +0100

    Cleans up test code

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,10 @@ pub mod interpreter;
 use lexer::{ Lexer, Token };
 use interpreter::{ Interpreter };
 
+
 fn main() {
     let source = "(let id (+ 2 (- 5 1)))".to_string();
-    let mut lexer = Lexer::new(source, 0);
-
-    loop {
-        let current = lexer.next();
-
-        if current == Token::EOF {
-            break;
-        }
-
-        match current {
-            Token::Identifier(identifier) => println!("Token: Identifier({})", identifier),
-            Token::Let => println!("Token: Keyword Let"),
-            Token::OpenParen => println!("Token: ("),
-            Token::CloseParen => println!("Token: )"),
-            Token::Number(number) => println!("Token: Number({})", number),
-            Token::Plus => println!("Token: +"),
-            Token::Minus => println!("Token: -"),
-            _ => println!("Unknown token"),
-        }
-    }
+    let lexer = Lexer::new(source, 0);
 
     let mut interpreter = Interpreter::new();
 
@@ -38,8 +20,6 @@ fn main() {
     interpreter.add_instruction(0);
     interpreter.add_instruction(1);
     interpreter.add_instruction(3);
-
-    print!("{:?}", interpreter);
 
     interpreter.run();
 }
